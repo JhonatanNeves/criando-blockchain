76e8fb80497bf4e9d8c9c52ad5e25351686989e3
jhonatanneves <nevesjn2@gmail.com>
2022-03-15 19:15:31 -0300

    first commit

--- blockchain.py
+++ blockchain.py
@@ -12,12 +12,11 @@ class Blockchain:
         self.create_block(proof=1, previous_hash='0')
 
     def create_block(self, proof, previous_hash):
-        block = {
-            'index': len(self.chain) + 1,
-            'timestamp': str(datetime.datetime.now()),
-            'proof': proof,
-            'previous_hash': previous_hash
-        }
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash
+                 }
         self.chain.append(block)
         return block
 
@@ -31,7 +30,7 @@ class Blockchain:
             hash_operation = hashlib.sha256(
                 str(new_proof**2 - previous_proof**2).encode()).hexdigest()
             if hash_operation[:4] == '0000':
-                check_proof == True
+                check_proof = True
             else:
                 new_proof += 1
                 return new_proof
@@ -43,7 +42,6 @@ class Blockchain:
     def is_chain_valid(self, chain):
         previous_block = chain[0]
         block_index = 1
-
         while block_index < len(chain):
             block = chain[block_index]
             if block['previous_hash'] != self.hash(previous_block):
